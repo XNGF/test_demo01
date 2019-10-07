21db7bcc05765e37e3f62d7b2bf0f5ecf4c32cff
Ref:rain <51809911+XNGF@users.noreply.github.com>
2019-10-07 20:18:32 +0800

    Update test_msg.py

--- 11-PO-msg/scripts/test_msg.py
+++ 11-PO-msg/scripts/test_msg.py
@@ -15,9 +15,9 @@ class TestMessage:
         time.sleep(3)
         self.driver.quit()
     @pytest.mark.parametrize(("phone","content"),[("18888888888","hello"),("13333333333","abc")])
-    def test_send_msg(self,phone,msg):
+    def test_send_msg(self,phone,content):
         self.page.msg_list.click_new_msg()
 
         self.page.new_msg.input_receiver(phone)
-        self.page.new_msg.input_msg(msg)
+        self.page.new_msg.input_msg(content)
         self.page.new_msg.click_send()
